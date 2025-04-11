0c8b23f5457187198be1857195b8e3cd0527392e
Loic Baron <loic.baron@gmail.com>
2025-04-11 09:37:32 +0200

    docs: add TrafficLight instructions

--- apps/frontend/timer/src/app/TrafficLight.tsx
+++ apps/frontend/timer/src/app/TrafficLight.tsx
@@ -1,3 +1,14 @@
+/*
+https://www.greatfrontend.com/questions/user-interface/traffic-light/react?framework=react
+Build a traffic light where the lights switch from green to yellow to red after predetermined intervals and loop indefinitely. Each light should be lit for the following durations:
+
+Red light: 4000ms
+Yellow light: 2000ms
+Green light: 3000ms
+
+You are free to exercise your creativity to style the appearance of the traffic light.
+*/
+
 import { useEffect, useState } from 'react';
 
 enum Colors {

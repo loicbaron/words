22b51fb57c71f78fc03e08923edd03da79fc2432
Loic Baron <loic.baron@gmail.com>
2025-04-11 09:11:54 +0200

    feat: add timer app

---
+++ apps/frontend/timer/src/app/TrafficLight.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+enum Colors {
+  Green = 'green',
+  Yellow = 'yellow',
+  Red = 'red',
+}
+
+type ColorProps = { next: Colors, wait: number }
+
+type ColorMap = {
+  'green': ColorProps;
+  'yellow': ColorProps;
+  'red': ColorProps
+}
+
+const colorsMap: ColorMap = {
+  'green': { next: Colors.Yellow, wait: 3000 },
+  'yellow': { next: Colors.Red, wait: 2000 },
+  'red': { next: Colors.Green, wait: 4000 },
+}
+
+export default function TrafficLight() {
+  const [color, setColor] = useState(Colors.Red);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setColor(colorsMap[color].next)
+    }, colorsMap[color].wait)
+    return (() => clearTimeout(id))
+  }, [color])
+  return <div>Traffic Light is {color}.</div>;
+}
---
+++ apps/frontend/timer/src/app/app.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+import TrafficLight from './TrafficLight';
+
+
+function useInterval(callback: () => void, delay: number | null): void {
+  const savedCallback = useRef<(() => void) | undefined>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval and clean up on unmount.
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id); // Cleanup on unmount or when delay changes
+    }
+    return undefined
+  }, [delay]);
+}
+
+
+export function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  useInterval(() => {
+    setSeconds(prev => prev + 1);  // Increment the seconds every 1000ms (1 second)
+  }, 1000);
+
+  return (
+    <div>
+      <p>Elapsed time: {seconds} seconds</p>
+      <p><TrafficLight /></p>
+    </div>
+  );
+}
+
+export default App;
